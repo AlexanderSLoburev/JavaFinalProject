78c002b50e9b3c5cbd0109703a69e531ea3fe2eb
Jack Raycoon <jack.o.raycoon@gmail.com>
2026-09-13 20:05:04 +0700

    refact(Task-39): add Javadoc to Interfaces

--- src/main/java/com/example/timsort/io/Command.java
+++ src/main/java/com/example/timsort/io/Command.java
@@ -1,6 +1,18 @@
 package com.example.timsort.io;
 
+/**
+ * A menu action.
+ *
+ * <p>Functional interface: implementations are typically provided as lambdas
+ * when registering menu options in {@link ConsoleMenu#register(int, String, Command)}.</p>
+ */
 @FunctionalInterface
 public interface Command {
+
+    /**
+     * Executes the action.
+     *
+     * <p>May throw {@link ExitException} to signal that the menu loop should stop.</p>
+     */
     void execute();
 }
